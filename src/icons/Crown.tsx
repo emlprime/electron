e13057ca8cd6a3d857e23cf9f38040cd241bdf33
Adan Garcia <madmendicant@gmail.com>
2024-05-20 20:18:08 -0700

    Updated Crown.tsx with new SVG Info, created new Flag.tsx icon.

--- src/icons/Crown.tsx
+++ src/icons/Crown.tsx
@@ -1,4 +1,22 @@
-export const Crown = ({height, width}) => <svg height={height} width={width} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-  <path d="M25 25 L50 10 L75 25 L90 50 L50 90 L10 50 Z" fill="gold" />
-  <path d="M30 30 L50 15 L70 30 L85 50 L50 85 L15 50 Z" fill="none" stroke="black" stroke-width="2" />
+import { JSX } from "solid-js/jsx-runtime"
+interface Props {
+  height: number;
+  width: number;
+}
+
+export const Crown = ({height, width}:Props) => <svg height={height} width={width} viewBox="0 0 18.866596 14.472462"
+version="1.1"
+id="svg1"
+xmlns="http://www.w3.org/2000/svg"
+>
+  <defs
+     id="defs1" />
+  <g
+     id="layer1"
+     transform="translate(-94.810084,-40.663516)">
+    <path
+       style="fill:#ffcc00;fill-opacity:1;stroke:#000000;stroke-width:0.25;stroke-dasharray:none;stroke-opacity:1"
+       d="m 96.985529,52.656356 -1.997367,-8.944736 4.168422,1.997368 5.123686,-4.863159 4.42895,5.297369 4.77633,-2.865788 -2.33663,9.726314 c 0,0 -2.00549,2.171054 -7.21602,1.99737 -5.167701,-0.172256 -6.947371,-2.344738 -6.947371,-2.344738 z"
+       id="path8" />
+  </g>
 </svg>
